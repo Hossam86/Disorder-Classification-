5dc781c06c4ba7747424ddd87b6302adb9464eb9
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 13:58:43 +0200

    transfor cat data

--- DisClassfic.py
+++ DisClassfic.py
@@ -4,3 +4,14 @@ from sklearn.model_selection import train_test_split
 from scipy.io import arff
 
 import pandas as pd
+
+data=arff.loadarff('DataSets\Autism-Adolescent-Data.arff')
+df=pd.DataFrame(data[0])
+list(df)
+
+#data preprocessing - transform catagorial data
+df=df.apply(lambda x:x.astype(str).str.lower())
+df.replace('yes',1)
+df.replace('no',0)
+df.replace('f',1)
+df.replace('m',0)
