cc8cdb34608558061e02970a11551c42de64b668
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 14:40:00 +0200

    build rf model

--- DisClassfic.py
+++ DisClassfic.py
@@ -8,18 +8,21 @@ import pandas as pd
 data=arff.loadarff('DataSets\Autism-Adolescent-Data.arff')
 df=pd.DataFrame(data[0])
 
+# df = df.select_dtypes([np.object]) 
+df = df.stack().str.decode('utf-8').unstack()
+
 #data preprocessing - transform catagorial data
 df=df.apply(lambda x:x.astype(str).str.lower())
-df.replace('yes',1)
-df.replace('no',0)
-df.replace('f',1)
-df.replace('m',0)
+df=df.replace('yes',1)
+df=df.replace('no',0)
+df=df.replace('f',1)
+df=df.replace('m',0)
 
 # subset the data
 xVar=list(df.loc[:,'A1_Score':'A10_Score'])+['gender']+['jundice']+['austim']
 yVar=df.iloc[:,20]
 df2=df[xVar]
-
+print(df2.head())
 
 # Split the Data into Train and Test Sets 
 
@@ -27,3 +30,9 @@ X_train,X_test,y_train,y_test=train_test_split(df2,yVar,test_size=0.2)
 
 # print(X_train.shape,y_train.shape)
 # print(X_test.shape,y_test.shape)
+
+# Build A random Forest 
+
+clf=RandomForestClassifier(n_estimators=100,n_jobs=-1, random_state=0)
+clf.fit(X_train,y_train)
+
